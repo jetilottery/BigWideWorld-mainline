6f1605046da3941c910776ce8e7183b6df9ab120
MYIGT\leew <warren.lee@igt.com>
2025-03-05 17:22:31 +0000

    Initial commit

---
+++ src/main/js/game/custom/textStyles.js
@@ -0,0 +1,12 @@
+define({
+  // Optional text style list in which to override template text styles
+  bonusWin: {
+    fontFamily: 'oswald',
+    fontSize: 44,
+    fontWeight: 'bold',
+    fill: 'fontColourBonusWin',
+    stroke: 0x0a518e,
+    strokeThickness: 7,
+    lineJoin: 'round',
+  },
+});
